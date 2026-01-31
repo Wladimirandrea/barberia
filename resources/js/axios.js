8170aadd490f7093b8470c76cfc9f5b9cd654693
Wladimirandrea <59346479+Wladimirandrea@users.noreply.github.com>
2026-01-31 13:19:46 -0600

    Update axios.js

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  const API = import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json'
   }
